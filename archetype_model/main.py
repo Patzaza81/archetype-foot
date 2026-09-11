2c7c7f12c794cea2de55240304d360ef0f23baa6
Patzaza81 <mouenguepatrick@gmail.com>
2026-09-11 14:53:02 +0100

    Update main.py

--- archetype_model/main.py
+++ archetype_model/main.py
@@ -42,7 +42,7 @@ from .signals import statistiques_signal
 from .signals import convergence
 from .signals import deduplication
 from .signals import selector
-from . import justification
+import justification
 from .edv import calculator as edv_calculator
 
 SCENARIOS = ("offensif", "defensif", "contextuel", "global")
